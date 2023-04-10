5833b14a6cfbb5118db11a0c95c4e878d9d40c7e
Thaetami <tatanghaetami.97@gmail.com>
2023-04-10 08:52:59 +0700

    update home page ui

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createApp } from 'vue'
 import 'flowbite'
 import './styles/style.css'
 import './script/globals/api-andpoint'
+import 'flowbite/dist/flowbite.js'
 import router from './script/router'
 import store from './script/views/Vuex'
 import App from './script/views/App.vue'
 import './styles/buttonTriger'
+
 
 
 createApp(App).use(store).use(router).mount('#app')
--- src/script/pages/Home.vue
+++ src/script/pages/Home.vue
@@ -8,42 +8,91 @@
         </div>
     </div>
 
-    <div class="bg-white gap-[5rem] grid lg:grid-cols-2 text-gray-300 min-h-screen p-10">
-      <div class="grid md:grid-cols-2 sm:p-3">
+    <div class="bg-white gap-[5rem] grid lg:grid-cols-2 text-gray-300 p-10"> 
+      <div class="grid md:grid-cols-2 sm:p-3 md:grid-start-2 md:border-double md:border-4">
+        <div class="inline sm:flex md:inline">
+          <img class="mr-6" src="https://placekitten.com/g/200/200">
+          <div class="mt-4 justify-between">
+            <button class="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Detail</button>
+          </div>
+        </div>
+
+        <div class="text-start lg:text-center">
+          <h4 class="mt-0 mb-2 uppercase text-black tracking-widest text-xs"></h4>
+          <h1 class="mt-0 mb-2 text-black text-4xl">Spotify Album Page with Tailwind CSS</h1>
+    
+          <p class="text-gray-600 mb-2 text-sm">With J. Cole, Quavo, Ty Dollar $ign</p>
+          <p class="text-gray-600 text-sm">Created by <a>Spotify</a> - 50 songs, 3 hr 2 min</p>
+        </div>
+      </div>
+
+      <div class="mt-0 w-full ">
+        <div class="flex text-black">
+          <div class="p-2 w-full">Title</div>
+          <div class="p-2 w-full">Artist</div>
+          <div class="p-2 w-full">Album</div>
+        </div>
+    
+        <div class="h-[300px] overflow-auto">
+          <div v-for="song in songs" v-bind:key="song.id" class="flex text-black hover:text-white border-b border-gray-800 hover:bg-gray-800">
+            <div class="p-3 w-full">{{ song.title }}</div>
+            <div class="p-3 w-full">{{ song.performer }}</div>
+            <div class="p-3 w-full">Spotify</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="justify-center flex mt-10 p-10">
+      <div class="carousel w-full sm:w-[80%] lg:-[60%] md:border-double md:border-4 md:border-black">
+        <div id="slide1" class="carousel-item relative w-full">
+          <div class="grid md:grid-cols-2 sm:p-3 border-slate-400 md:grid-start-2">
+            <div class="inline sm:flex md:inline">
+              <img class="mr-6" src="https://placekitten.com/g/200/200">
+              <div class="mt-4 justify-between">
+                <button class="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Detail</button>
+              </div>
+            </div>
+
+            <div class="text-start lg:text-center">
+              <h4 class="mt-0 mb-2 uppercase text-black tracking-widest text-xs"></h4>
+              <h1 class="mt-0 mb-2 text-black text-4xl">Spotify Album Page with Tailwind CSS</h1>
+      
+              <p class="text-gray-600 mb-2 text-sm">With J. Cole, Quavo, Ty Dollar $ign</p>
+              <p class="text-gray-600 text-sm">Created by <a>Spotify</a> - 50 songs, 3 hr 2 min</p>
+            </div>
+        </div>
+          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" class="btn btn-circle">❮</a> 
+            <a href="#slide2" class="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" class="carousel-item relative w-full">
+          <div class="grid md:grid-cols-2 sm:p-3 border-slate-400 md:grid-start-2">
           <div class="inline sm:flex md:inline">
             <img class="mr-6" src="https://placekitten.com/g/200/200">
             <div class="mt-4 justify-between">
-                <button class="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Detail</button>
+              <button class="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Detail</button>
             </div>
           </div>
 
           <div class="text-start lg:text-center">
             <h4 class="mt-0 mb-2 uppercase text-black tracking-widest text-xs"></h4>
             <h1 class="mt-0 mb-2 text-black text-4xl">Spotify Album Page with Tailwind CSS</h1>
-      
+    
             <p class="text-gray-600 mb-2 text-sm">With J. Cole, Quavo, Ty Dollar $ign</p>
             <p class="text-gray-600 text-sm">Created by <a>Spotify</a> - 50 songs, 3 hr 2 min</p>
           </div>
+        </div>
+          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" class="btn btn-circle">❮</a> 
+            <a href="#slide3" class="btn btn-circle">❯</a>
+          </div>
+        </div> 
       </div>
-
-      <div class="mt-0 w-full row-start-1 md:row-start-none">
-        <div class="flex text-black">
-          <div class="p-2 w-full">Title</div>
-          <div class="p-2 w-full">Artist</div>
-          <div class="p-2 w-full">Album</div>
-        </div>
-  
-        <div class="h-[300px] overflow-auto">
-        <div v-for="song in songs" v-bind:key="song.id" class="flex text-black hover:text-white border-b border-gray-800 hover:bg-gray-800">
-          <div class="p-3 w-full">{{ song.title }}</div>
-          <div class="p-3 w-full">{{ song.performer }}</div>
-          <div class="p-3 w-full">Spotify</div>
-        </div>
-        </div>
-
-      </div>
-      
     </div>
+
 </template>
 
 <script>
