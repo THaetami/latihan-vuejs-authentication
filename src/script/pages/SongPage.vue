5b899783850c204efe6e4349f8e6220c5dccd312
Thaetami <tatanghaetami.97@gmail.com>
2023-04-19 15:50:35 +0700

    update form validation song upload

--- src/script/pages/SongPage.vue
+++ src/script/pages/SongPage.vue
@@ -4,28 +4,33 @@
             <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Upload Your Song</h3>
             <form @submit.prevent="postSong" class="space-y-4 " >
                 <div class="relative z-0 w-full mb-6 group">
-                    <input v-model="title" type="text" name="title" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input v-model.trim="title" @input="validateTitle" type="text" name="title" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
                     <label for="title" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
+                    <small v-if="titleErrorMessage" class="text-red-500">{{ titleErrorMessage }}</small>
                 </div>
 
                 <div class="grid grid-cols-2 md:gap-6">
                     <div class="relative z-0 w-full mb-6 group">
-                        <input v-model="performer" type="text" name="performer" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input v-model.trim="performer" @input="validatePerformer" type="text" name="performer" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
                         <label for="performer" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Performer</label>
+                        <small v-if="performerErrorMessage" class="text-red-500">{{ performerErrorMessage }}</small>
                     </div>
                     <div class="relative z-0 w-full mb-6 group">
-                        <input v-model="year" type="number" name="year" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input v-model.trim="year" @input="validateYear" type="number" name="year" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
                         <label for="year" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Year</label>
+                        <small v-if="yearErrorMessage" class="text-red-500">{{ yearErrorMessage }}</small>
                     </div>
                 </div>
                 <div class="grid grid-cols-2 md:gap-6">
                     <div class="relative z-0 w-full mb-6 group">
-                        <input v-model="genre" type="text" name="genre" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input v-model.trim="genre" @input="validateGenre" type="text" name="genre" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
                         <label for="genre" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Genre</label>
+                        <small v-if="genreErrorMessage" class="text-red-500">{{ genreErrorMessage }}</small>
                     </div>
                     <div class="relative z-0 w-full mb-6 group">
-                        <input v-model="duration" type="number" name="duration" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input v-model.trim="duration" @input="validateDuration" type="number" name="duration" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
                         <label for="duration" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Duration</label>
+                        <small v-if="durationErrorMessage" class="text-red-500">{{ durationErrorMessage }}</small>
                     </div>
                 </div>
 
@@ -108,7 +113,12 @@ export default {
             year: '',
             genre: '',
             duration: '',
-            id: ''
+            id: '',
+            titleErrorMessage: '',
+            performerErrorMessage: '',
+            yearErrorMessage: '',
+            genreErrorMessage: '',
+            durationErrorMessage: ''
         }
     },
     watch: {
@@ -126,22 +136,79 @@ export default {
             const response = await axios.get(`songs/${this.$route.params.songId}`)
             this.song = response.data.data.song
         },
+        validateTitle() {
+            if (this.title.length < 3) {
+                this.titleErrorMessage = 'min. 3 karakter.';
+                return false;
+            } else {
+                this.titleErrorMessage = '';
+                return true;
+            }
+        },
+        validatePerformer() {
+            if (this.performer.length < 3) {
+                this.performerErrorMessage = 'min. 3 karakter.';
+                return false;
+            } else {
+                this.performerErrorMessage = '';
+                return true;
+            }
+        },
+        validateYear() {
+            if (!/^\d{4}$/.test(this.year)) {
+                this.yearErrorMessage = '4 digit karakter numerik.';
+                return false;
+            } else {
+                this.yearErrorMessage = '';
+                return true;
+            }
+        },
+        validateGenre() {
+            if (this.genre.length < 3) {
+                this.genreErrorMessage = 'min. 3 karakter.';
+                return false;
+            } else {
+                this.genreErrorMessage = '';
+                return true;
+            }
+        },
+        validateDuration() {
+            if (!/\d/.test(this.duration)) {
+                this.durationErrorMessage = 'min. 1 karakter numerik.';
+                return false;
+            } else {
+                this.durationErrorMessage = '';
+                return true;
+            }
+        },
         async postSong() {
-            const response = await axios.post('songs', {
-                title: this.title.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '),
-                year: this.year,
-                performer: this.performer,
-                genre: this.genre,
-                duration: this.duration
-            })
-            this.id = response.data.data.songId
-            this.showModalAddSong = false
-            this.title = ''
-            this.year = ''
-            this.performer = ''
-            this.genre = ''
-            this.duration = ''
-        }
+            const isTitleValid = this.validateTitle();
+            const isPerformerValid = this.validatePerformer();
+            const isYearValid = this.validateYear();
+            const isGenreValid = this.validateGenre();
+            const isDuration = this.validateDuration();
+
+            if (isTitleValid && isPerformerValid && isYearValid && isGenreValid && isDuration) {
+                try {
+                    const response = await axios.post('songs', {
+                        title: this.title.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '),
+                        year: this.year,
+                        performer: this.performer,
+                        genre: this.genre,
+                        duration: this.duration
+                    })
+                    this.id = response.data.data.songId
+                    this.showModalAddSong = false
+                    this.title = ''
+                    this.year = ''
+                    this.performer = ''
+                    this.genre = ''
+                    this.duration = ''
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        },
     }
 }
 </script>
